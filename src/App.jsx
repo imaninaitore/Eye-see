16f0067f48a75e5a69b77ef7612cbc1c8c5522af
imaninaitore <imaninaitore1726@gmail.com>
2026-07-04 22:18:15 +0300

    import product list to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './assets/components/Navbar'
+import Footer from './assets/components/footer'
+import ProductList from './assets/components/ProductList'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Footer/>
+    <ProductList/>
     </>
   )
 }
