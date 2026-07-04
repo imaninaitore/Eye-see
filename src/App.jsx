348672ee13506c8941927afd2b17ed78af85ccf8
imaninaitore <imaninaitore1726@gmail.com>
2026-07-04 23:39:44 +0300

    set image background in header section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './assets/components/Navbar'
 import Footer from './assets/components/footer'
 import ProductList from './assets/components/ProductList'
+import Header from './assets/components/Header'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Header/>
     <ProductList/>
     <Footer/>
     </>
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import background from "@/assets/bg.jpg"
+
+function Header() {
+  return (
+    <header style={{
+        backgroundImage:`url(${background})`,
+        backgroundSize:"cover",
+        backgroundPosition:"center",
+        height: "500px",
+        }}>
+
+        <h1 className="text-cyan-300 text-3xl sm:text-2xl md:text-5xl lg:text-6xl  py-6 px-6">Welcome to Eye see shop!</h1>
+        <p className="text-white text-2xl sm:text-2xl md:text-3xl lg:text-4xl  py-6 px-6">where quality meets clarity</p>
+    </header>
+  )
+}
+
+export default Header
