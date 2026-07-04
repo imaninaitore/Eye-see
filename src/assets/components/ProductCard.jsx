91ea8c595b1d1248d2e0118cabc6739ed04f3d16
imaninaitore <imaninaitore1726@gmail.com>
2026-07-04 21:53:12 +0300

    write card structure with reference to objects from the products json file

--- src/assets/components/ProductCard.jsx
+++ src/assets/components/ProductCard.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import { Card,CardContent,CardTitle,CardHeader } from './ui/card'
 
-function ProductCard() {
+function ProductCard({id, name, price, category, description, image }) {
   return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+        <h2>{name}</h2>  
+        </CardTitle>
+      </CardHeader >
+      <CardContent>
 
+      <img src={image} alt={name} width="200" />    
+
+      <p>Price: Ksh. {price}</p>
+      <p>{description}</p>
+      <p>Category: {category}</p>
+      <Button variant='default' size="xs">Add to cart</Button>
+      </CardContent>
+     </Card>
   )
 }
 
