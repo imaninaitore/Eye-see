240e126c6dd95175ce431c7eb101beb7c3f9b200
imaninaitore <imaninaitore1726@gmail.com>
2026-07-05 18:31:25 +0300

    import router

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Navbar from './assets/components/Navbar'
 import Footer from './assets/components/footer'
 import ProductList from './assets/components/ProductList'
 import Header from './assets/components/Header'
-
+import { BrowserRouter as Router , Routes,Route } from 'react-router-dom'
+import ProductDetails from './assets/components/ProductDetails'
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <Router>
+      <Navbar/>
+      <Route path='/product/:productId' element={<ProductDetails/>}/>
     <Header/>
       <ProductList/>
     <Footer/>
+    </Router>
+    
     </>
   )
 }
