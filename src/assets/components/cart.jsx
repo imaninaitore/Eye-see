09fffc7076c004651b039426ed345d0768bb43bf
imaninaitore <imaninaitore1726@gmail.com>
2026-07-05 13:42:44 +0300

    add  function for calculation of total in cart,removing items and clearing the cart

--- src/assets/components/cart.jsx
+++ src/assets/components/cart.jsx
@@ -4,10 +4,16 @@ function Cart({onCheckout}) {
     const {cart , setcart} = useContext(cartContext)
     const navigate = useNavigate();
 
+    const totalPrice = cart.reduce(
+        (sum,item) => sum + (item.price * (item.quantity || 1)), 0);
     
-
-
-
+    const handleRemoveItem = (itemId) => {
+    const updatedCart = cart.filter(item => item.id !== itemId);
+    setCart(updatedCart);
+    }      
+    const handleClearCart = () => {
+    setCart([]);
+    }
 
   return (
     <div>Cart</div>
