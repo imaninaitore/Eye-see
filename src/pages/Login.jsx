c3ef33226c74cb2ee432320e4163ad01b9de9534
imaninaitore <imaninaitore1726@gmail.com>
2026-07-07 17:18:50 +0300

    import needed components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useAuth } from '@/context/AuthContext';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Eye, EyeOff, Loader2 } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+
 
 function Login() {
   return (
