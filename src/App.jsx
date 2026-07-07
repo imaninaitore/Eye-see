c44fddb96e88a9747ffe03c3dd90959296a2610e
imaninaitore <imaninaitore1726@gmail.com>
2026-07-07 17:56:50 +0300

    import and link login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Footer from './pages/Footer'
 import ProductList from './components/ProductList'
 import Header from './components/Header'
 import { BrowserRouter as Router , Routes,Route } from 'react-router-dom'
 import ProductDetails from './components/ProductDetails'
+import { Login } from './pages/login'
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
          <Footer/>
         </>
       }/>
+        <Route path="/login" element={<Login/>}/>
+
       <Route path='/shop' element={
         <>
        <ProductList/> 
