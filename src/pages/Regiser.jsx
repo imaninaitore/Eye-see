bc2dcaf93f09f1563c09ec1aa38e04be5ce7ac5e
imaninaitore <imaninaitore1726@gmail.com>
2026-07-08 09:32:37 +0300

    write functions responsible for collecting form data , submitting and the navigation function

---
+++ src/pages/Regiser.jsx
@@ -0,0 +1,20 @@
+import { useAuth } from '@/context/AuthContext'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+export const Register = () => {
+    const { register } = useAuth;
+    const [formData, setFormData] = useState({name:'',email:'',password:'', confirmedPassword:''});
+    const [error,settError] =useState('');
+    const [isSubmitting,setIsSubmitting] = useState(false);
+    const [showPassword, setShowPassword] = useState(false)
+    const navigate = useNavigate();
+
+    
+  
+  
+    return (
+  )
+}
+
+export default Regiser
